d8b8b148759ac961592dd29b2d6c37bfb270c567
Williams Joseph <wj882244@gmail.com>
2024-02-26 09:07:02 +0100

    learning about classes in php

---
+++ OOP/Car.php
@@ -0,0 +1,10 @@
+<?php
+
+class Car {
+
+    // private class means that this variable can only be accessed inside this class
+    //protected means it can be accessed by it child class not only this class 
+    //public it can be accessed everywhere 
+    private $brand = "toyota";
+    public $color = "blue";
+}
